4c6af9b341b2eaa7a9373145b8a7c339916b861c
rikiweb <riccardolambrugo@gmail.com>
2019-06-05 00:42:41 +0200

    V5 con corsi
    V5 con cambiamenti iniziali sulla parte get e set dei corsi salvati nell'account sul db

---
+++ Es_Finale_Checkbox/config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', '34.218.240.251:3306');
+   define('DB_USERNAME', 'ecdl');
+   define('DB_PASSWORD', 'dili1234');
+   define('DB_DATABASE', 'cartonato');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
